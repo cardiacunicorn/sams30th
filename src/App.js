792bf9fac06f362e91ea6514716c0c3459c7ff1f
Alex Mills <alexanderwmills@gmail.com>
2020-03-27 20:13:38 +1100

    initial mockup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './sambo.png';
+import house from './house-sideview.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet"></link>
+      <header className="grid-container">
+        <div class="grid-item item1"><img src={logo} className="App-logo sam" alt="logo" /></div>
+        <div class="grid-item item2"><h1>Sam's 30th Birthday House Party!</h1></div>
+        <div class="grid-item item3"><img src={logo} className="App-logo sam" alt="logo" /></div>
       </header>
+      <body className="App-body">
+        <div className="housegrid">
+          <a href="https://zoom.com">
+          <div class="grid-item houseitem1">click me</div></a>
+          <div class="grid-item houseitem2"></div>
+          <div class="grid-item houseitem3"></div>
+        </div>
+      </body>
     </div>
   );
 }
