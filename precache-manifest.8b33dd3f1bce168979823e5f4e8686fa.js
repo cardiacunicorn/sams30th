98894db1e12519ce2d0c2bd2982fcdec1b77c667
Alex Mills <alexanderwmills@gmail.com>
2020-03-29 17:26:44 +1100

    Updates

---
+++ precache-manifest.8b33dd3f1bce168979823e5f4e8686fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1c88937355da9e1760e3e4ff1ae0d8",
+    "url": "/sams30th/index.html"
+  },
+  {
+    "revision": "fbc2a29c2703f89fffe2",
+    "url": "/sams30th/static/css/main.4513adf8.chunk.css"
+  },
+  {
+    "revision": "bd06a9c1e542c2ae7e2a",
+    "url": "/sams30th/static/js/2.84ef4a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sams30th/static/js/2.84ef4a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc2a29c2703f89fffe2",
+    "url": "/sams30th/static/js/main.44af98e8.chunk.js"
+  },
+  {
+    "revision": "cb06f99b5e2de2526cf7",
+    "url": "/sams30th/static/js/runtime-main.d838dcf4.js"
+  },
+  {
+    "revision": "73aac4bc35ebfefaf4a14d3d6367308d",
+    "url": "/sams30th/static/media/house-sideview.73aac4bc.png"
+  },
+  {
+    "revision": "5bbced1782bf528d360fd0fbae5244e4",
+    "url": "/sams30th/static/media/sambo.5bbced17.png"
+  }
+]);
